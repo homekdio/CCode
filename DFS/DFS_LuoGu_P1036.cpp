d147fb805fabbfe9cd36ffebc8d11225b277e991
homekdio <1813742772@qq.com>
2023-03-31 16:11:25 +0800

    DFS_洛谷_P1036 选数

--- DFS/DFS_LuoGu_P1036.cpp
+++ DFS/DFS_LuoGu_P1036.cpp
@@ -1,7 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n,k,res=0;
+int arr[21],st[21];
+bool is_prime(int sum)
+{
+    if(sum<2) return false;
+    for(int i=2;i<=sqrt(sum);i++)
+    {
+        if(sum%i==0)
+        return false;
+    }
+    return true;
+}
+void dfs(int x,int start)
+{
+    if(x>k)
+    {
+        int sum=0;
+        for(int i=1;i<=k;i++)
+        sum+=st[i];
+        if(is_prime(sum))
+        {
+            res++;
+        }
+        return ;
+    }
+    for(int i=start;i<=n;i++)
+    {   
+        st[x]=arr[i];
+        dfs(x+1,i+1);
+        st[x]=0;
+    }
+}
 int main()
 {
-
+    cin>>n>>k;
+    for(int i=1;i<=n;i++)
+    cin>>arr[i];
+    dfs(1,1);
+    cout<<res;
     return 0;
 }
