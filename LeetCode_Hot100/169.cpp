dae270feeb83b5ef5a229c151b6fb09a6bce8d62
homekdio <1813742772@qq.com>
2023-06-30 10:52:29 +0800

    递归排序

--- LeetCode_Hot100/169.cpp
+++ LeetCode_Hot100/169.cpp
@@ -1,21 +1,26 @@
+// 这是递归排序
 class Solution {
-void quick_sort(vector<int>& num,int l,int r)
+void merge_sort(vector<int>& num,int l,int r)
 {
     if(l>=r) return;
-    int x=num[l],i=l-1,j=r+1;
-    while(i<j)
+    int mid=(l+r)/2;
+    merge_sort(num,l,mid);
+    merge_sort(num,mid+1,r);
+    int k=0,i=l,j=mid+1;
+    int tmp[r+10];
+    while(i<=mid&&j<=r)
     {
-        do i++; while(num[i]<x);
-        do j--; while(num[j]>x);
-        if(i<j) swap(num[i],num[j]);
+        if(num[i]<num[j]) tmp[k++]=num[i++];
+        else tmp[k++]=num[j++]; 
     }
-    quick_sort(num,l,j);
-    quick_sort(num,j+1,r);
+    while(i<=mid) tmp[k++]=num[i++];
+    while(j<=r) tmp[k++]=num[j++];
+    for(i=l,j=0;i<=r;i++,j++) num[i]=tmp[j];
 }
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
-        quick_sort(nums,0,n-1);
+        merge_sort(nums,0,n-1);
         return nums[n/2];
     }
 };
