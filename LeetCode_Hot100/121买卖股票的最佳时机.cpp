38b785ea1d228722ae5047f44d428d5f7adb5d4c
homekdio <1813742772@qq.com>
2023-08-23 16:43:26 +0800

    hot100练习

---
+++ LeetCode_Hot100/121买卖股票的最佳时机.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    int minprice = 1e9,maxprofit = 0;
+    for(int price:prices)
+    {
+        maxprofit = max(maxprofit,price-minprice);
+        minprice = min(minprice,price);
+    }
+    return maxprofit;
+    }
+};
